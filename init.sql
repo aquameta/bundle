f049e0950f409e47d362389d0bb9e19e71b46106
Eric Hanson <eric@aquameta.com>
2023-12-14 16:20:08 +0000

    delta.exec()

--- init.sql
+++ init.sql
@@ -9,7 +9,21 @@ create extension if not exists pgcrypto schema public;
 create extension if not exists meta version '0.5.0';
 create extension if not exists meta_triggers;
 
-begin;
+-- begin;
 
 create schema delta;
 set search_path=delta;
+
+
+
+-- TODO: where does this go?
+create or replace function exec(statements text[]) returns setof record as $$
+   declare
+       statement text;
+   begin
+       foreach statement in array statements loop
+           raise debug 'EXEC statement: %', statement;
+           return query execute statement;
+       end loop;
+    end;
+$$ language plpgsql volatile returns null on null input;
