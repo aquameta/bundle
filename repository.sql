cc373c15ba66a94f34b651dca78efc3b7d0d9810
Eric Hanson <eric@aquameta.com>
2024-10-22 20:01:41 -0500

    clean up repository.sql and tests a bit

--- repository.sql
+++ repository.sql
@@ -77,12 +77,37 @@ create table repository (
 alter table commit add column repository_id uuid /* not null FIXME why is deferrable not working?? */ references repository(id) on delete cascade deferrable initially deferred;
 
 
-------------------------------------------------------------------------------
--- FUNCTIONS
-------------------------------------------------------------------------------
+/*
+--
+-- migrations
+--
+
+create table commit_migration (
+    id uuid not null default public.uuid_generate_v7() primary key,
+    commit_id uuid not null references commit(id),
+    up_code text,
+    down_code text, -- can we auto-generate a lot of this?
+    before_checkout boolean,
+    ordinal_position integer
+);
+
 
 --
--- id()
+-- dependencies
+--
+
+create table dependency (
+    id uuid not null default public.uuid_generate_v7() primary key
+);
+*/
+
+
+-------------------------------
+-- Name/id functions
+-------------------------------
+
+--
+-- repository_id()
 --
 
 create or replace function repository_id( repository_name text ) returns uuid as $$
@@ -100,22 +125,6 @@ $$ stable language sql;
 
 
 --
--- stage_commit_id()
---
-
-/*
-MOVED TO repo.stage
-create or replace function _stage_commit_id( repository_id uuid ) returns uuid as $$
-    select stage_commit_id from delta.repository where id=repository_id;
-$$ stable language sql;
-
-create or replace function stage_commit_id( repository_name text ) returns uuid as $$
-    select stage_commit_id from delta.repository where name=repository_name;
-$$ stable language sql;
-*/
-
-
---
 -- head_commit_id()
 --
 
@@ -126,7 +135,6 @@ $$ stable language sql;
 create or replace function head_commit_id( repository_name text ) returns uuid as $$
     select head_commit_id from delta.repository where name=repository_name;
 $$ stable language sql;
-
 
 
 --
@@ -141,6 +149,10 @@ create or replace function checkout_commit_id( repository_name text ) returns uu
     select checkout_commit_id from delta.repository where name=repository_name;
 $$ stable language sql;
 
+
+-------------------------------
+-- operation functions
+-------------------------------
 
 --
 -- create_repository()
@@ -159,27 +171,8 @@ begin
         raise exception 'Repository name cannot be null.';
     end if;
 
-/*
-    -- create the repo's stage_commit
-    insert into delta.commit (manifest) values
-    ('{
-        "tracked_rows_added": [
-        ],
-        "stage_rows_added": [
-        ],
-        "commit_rows": [
-        ]
-    }')
-    returning id into _stage_commit_id;
-*/
-
     -- create repository
     insert into delta.repository (name) values (repository_name) returning id into _repository_id;
-
-    /*
-    -- point stage_commit at repository
-    update delta.commit set repository_id=_repository_id where id=_stage_commit_id;
-    */
 
     return _repository_id;
 exception
@@ -215,6 +208,27 @@ create or replace function delete_repository( repository_name text ) returns voi
     end;
 $$ language plpgsql;
 
+
+--
+-- garbage_collect()
+--
+
+/*
+create or replace function garbage_collect() returns setof text as $$
+    delete from delta.blob
+    using (
+        select b.hash as bad_hash from delta.blob b
+            left join GONE: delta.commit_field_changed cfc on cfc.value_hash = b.hash
+        where  cfc.value_hash is null
+    )
+    where hash = bad_hash
+    returning bad_hash
+$$ language sql;
+*/
+
+-------------------------------
+-- info functions
+-------------------------------
 
 --
 -- repository_exists()
@@ -259,7 +273,7 @@ $$ language plpgsql;
 
 
 --
--- _commit_exists()
+-- commit_exists()
 --
 
 create or replace function _commit_exists(commit_id uuid) returns boolean as $$
@@ -267,112 +281,8 @@ create or replace function _commit_exists(commit_id uuid) returns boolean as $$
 $$ language sql;
 
 
-/*
 --
--- commit_rows()
---
-
-
--- cache checker, divert to head_commit_row mat view if possible
-TRASH:
-create or replace function commit_rows(_commit_id uuid, _relation_id_filter meta.relation_id default null)
-returns table(commit_id uuid, row_id meta.row_id) as $$
-declare
-    is_cached boolean;
-begin
-    select into is_cached exists (select 1 from delta.head_commit_row hcr where hcr.commit_id = _commit_id);
-
-    raise debug 'commit_rows(%, %): is_cached: %', _commit_id, _relation_id_filter, is_cached;
-
-    if not is_cached or false then
-        return query select * from delta._commit_rows(_commit_id, _relation_id_filter);
-    else
-        return query select hcr.commit_id, hcr.row_id --, position
-        from delta.head_commit_row hcr
-        where hcr.row_id::meta.relation_id =
-            case
-                -- no op
-                when _relation_id_filter is null then hcr.row_id::meta.relation_id
-                -- filter
-                else _relation_id_filter
-            end
-        and hcr.commit_id = _commit_id;
-    end if;
-end
-$$ language plpgsql;
-*/
-
-
-
-/*
-recursive cte, traverses commit ancestry, grabbing added rows and removing rows deleted
-
-- get the ancestry tree of the commit being materialized, in a cte
-- with ancestry, start with the root commit and move forward in time
-- stop at releases!
-- for each commit
-    - add rows added
-    - remove rows deleted
-
--- NOTE:
--- How the heck do I write a function that returns a record with one column whose type is meta.row_id?
--- If I do table(row_id meta.row_id) it thinks I'm passing in a type and returns all fields of row_id as separate columns.
--- If I do setof meta.row_id it does the same.
--- Adding (useless) commit_id to return type to fix
-
-TRASH.  Now commits are not additive, the manifest holds the whole thing so no CTE.
-
-create or replace function _commit_rows( _commit_id uuid, _relation_id meta.relation_id default null ) returns table(commit_id uuid, row_id meta.row_id) as $$
-    with recursive ancestry as (
-        select c.id as commit_id, c.parent_id, 0 as position from delta.commit c where c.id=_commit_id
-        union
-        select c.id as commit_id, c.parent_id, p.position + 1 from delta.commit c join ancestry p on c.id = p.parent_id
-    ),
-    -- every added row, with its position in the ancestry tree
-    rows_added as (
-        select min(a.position) as newest_position, cra.row_id
-        from ancestry a
-            join delta.commit_row_added cra on cra.commit_id = a.commit_id
-        group by cra.row_id
-    ),
-    -- every row deleted, with its position in the ancestry tree
-    rows_deleted as (
-        select min(a.position) as newest_position, crd.row_id
-        from ancestry a
-            join delta.commit_row_deleted crd on crd.commit_id = a.commit_id
-            group by crd.row_id
-    )
-    -- WIP
-    select
-        _commit_id,
-        ra.row_id / * as added_row_id,
-        ra.newest_position as added_position,
-        rd.row_id as deleted_row_id,
-        rd.newest_position * /
-    from rows_added ra
-        left join rows_deleted rd on rd.row_id = ra.row_id
-    where (
-        -- never deleted
-        rd.row_id is null
-        or
-        -- deleted and re-added
-        rd.newest_position >= ra.newest_position
-    )
-
-    and
-        -- relation filter, if passed in
-        (ra.row_id)::meta.relation_id =
-            case
-                when _relation_id is not null then _relation_id
-                else (ra.row_id)::meta.relation_id
-            end;
-    return;
-$$ language plpgsql;
-*/
-
-
---
--- commit_rows()
+-- get_commit_rows()
 --
 
 create or replace function _get_commit_rows( _commit_id uuid, _relation_id meta.relation_id default null ) returns table(commit_id uuid, row_id meta.row_id) as $$
@@ -392,12 +302,11 @@ $$ language sql;
 
 
 --
--- commit_fields()
+-- get_commit_fields()
 --
+-- returns a field and it's value hash
 
--- a field and it's value hash
 create type field_hash as ( field_id meta.field_id, value_hash text);
-
 create or replace function _get_commit_fields(_commit_id uuid, _relation_id_filter /* TODO */ meta.relation_id default null)
 returns setof field_hash as $$
     select meta.field_id(
@@ -420,24 +329,6 @@ create or replace function _get_head_commit_fields( _repository_id uuid ) return
 $$ language sql;
 
 
-
-
-
-
-/*
-create or replace function get_head_commit_rows( repository_name text default null ) returns table(commit_id uuid, row_id meta.row_id) as $$
-declare
-    repository_id uuid;
-begin
-    if repository_name is null then 
-        return query select * from delta._get_head_commit_rows();
-    else
-        return query select * from delta._get_head_commit_rows(delta.repository_id(repository_name));
-    end if;
-end;
-$$ language plpgsql;
-*/
-
 --
 -- get_commit_manifest()
 --
@@ -445,45 +336,3 @@ $$ language plpgsql;
 create or replace function _get_commit_manifest( _commit_id uuid ) returns jsonb as $$
     select manifest from delta.commit where id = _commit_id;
 $$ language sql;
-
-
---
--- garbage_collect()
---
-
-/*
-create or replace function garbage_collect() returns setof text as $$
-    delete from delta.blob
-    using (
-        select b.hash as bad_hash from delta.blob b
-            left join GONE: delta.commit_field_changed cfc on cfc.value_hash = b.hash
-        where  cfc.value_hash is null
-    )
-    where hash = bad_hash
-    returning bad_hash
-$$ language sql;
-*/
-
-
-/*
---
--- migrations
---
-
-create table commit_migration (
-    id uuid not null default public.uuid_generate_v7() primary key,
-    commit_id uuid not null references commit(id),
-    up_code text,
-    down_code text, -- can we auto-generate a lot of this?
-    before_checkout boolean,
-    ordinal_position integer
-);
-
---
--- dependencies
---
-
-create table dependency (
-    id uuid not null default public.uuid_generate_v7() primary key
-);
-*/
--- test/repository.sql
+++ test/repository.sql
@@ -1,9 +1,41 @@
-insert into delta.blob(value) values('hi mom');
+DO $$ BEGIN RAISE NOTICE '----------- repository.sql ------------'; END $$;
 
+--
+-- blob table and hashing
+--
+
+insert into delta.blob(value) values('hi mom');
 select results_eq(
     $$ select hash from delta.blob where value='hi mom' $$,
-    $$ select digest('hi mom', 'sha256')::text; $$,
-    'Blob hash equals digest() output.'
+    $$ select delta.hash('hi mom')::text; $$,
+    'Blob hash of val < 32 chars equals delta.hash() output.'
+);
+
+insert into delta.blob(value) values('this is a very long string that is longer than 32 chars');
+select results_eq(
+    $$ select hash from delta.blob where value='this is a very long string that is longer than 32 chars' $$,
+    $$ select delta.hash('this is a very long string that is longer than 32 chars')::text; $$,
+    'Blob hash > 32 chars equals hash() output.'
+);
+
+select results_eq(
+    $$ select 'testing'; $$,
+    $$ select delta.unhash(delta.hash('testing')); $$,
+    'Unhash of hashed output equals input value'
+);
+
+/*
+select results_eq(
+    $$ select 'abcdefghijklmnopqrstuvwxyz1234567890'; $$,
+    $$ select delta.unhash(delta.hash('abcdefghijklmnopqrstuvwxyz1234567890')); $$,
+    'Unhash of hashed output equals input value'
+);
+*/
+
+select is(
+    null,
+    delta.unhash(delta.hash(null)),
+    'Unhash of hash of null is null'
 );
 
 -----------------------------------------------------------
@@ -63,8 +95,36 @@ select ok(
 
 -----------------------------------------------------------
 
+delete from delta.repository where name='org.opensourceshakespeare.db';
 select delta.create_repository('org.opensourceshakespeare.db');
 select ok(
     delta.repository_exists('org.opensourceshakespeare.db'),
     'repository_exists() finds an existing repository'
 );
+
+
+/*
+List of functions in repository.sql as of 10/22/2024:
+
+_blob_hash_gen_trigger() returns trigger as $$
+repository_id( repository_name text ) returns uuid as $$
+_repository_name( repository_id uuid ) returns text as $$
+_head_commit_id( repository_id uuid ) returns uuid as $$
+head_commit_id( repository_name text ) returns uuid as $$
+_checkout_commit_id( repository_id uuid ) returns uuid as $$
+checkout_commit_id( repository_name text ) returns uuid as $$
+create_repository( repository_name text ) returns uuid as $$
+_delete_repository( repository_id uuid ) returns void as $$
+delete_repository( repository_name text ) returns void as $$
+garbage_collect() returns setof text as $$
+repository_exists( _name text ) returns boolean as $$
+_repository_exists( repository_id uuid ) returns boolean as $$
+_repository_has_commits( _repository_id uuid ) returns boolean as $$
+_repository_has_uncommitted_changes( _repository_id uuid ) returns boolean as $$
+_commit_exists(commit_id uuid) returns boolean as $$
+_get_commit_rows( _commit_id uuid, _relation_id meta.relation_id default null ) returns table(commit_id uuid, row_id meta.row_id) as $$
+_get_head_commit_rows( _repository_id uuid ) returns table(commit_id uuid, row_id meta.row_id) as $$
+_get_commit_fields(_commit_id uuid, _relation_id_filter meta.relation_id default null)
+_get_head_commit_fields( _repository_id uuid ) returns setof field_hash as $$
+_get_commit_manifest( _commit_id uuid ) returns jsonb as $$
+*/
