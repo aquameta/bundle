dd8d6a7c2356566a2a4d1f59cb53dafeca3c1ab5
Eric Hanson <eric@aquameta.com>
2024-10-30 01:16:37 -0500

    rename ignore.sql to trackable.sql
    now includes
    - ignore tables
    - ignore functions
    - trackable_nontable_relation table
    - track_nontable function

--- vi.sh
+++ vi.sh
@@ -1,1 +1,1 @@
-vi -p init.sql repository.sql db.sql ignore.sql track.sql stage.sql commit.sql checkout.sql remote.sql merge.sql import-export.sql search.sql status.sql
+vi -p init.sql repository.sql db.sql trackable.sql track.sql stage.sql commit.sql checkout.sql remote.sql merge.sql import-export.sql search.sql status.sql
