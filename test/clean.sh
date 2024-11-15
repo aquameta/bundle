8cb6e9615caa25ea98e9d37527ccf491d50a2b06
Eric Hanson <eric@aquameta.com>
2024-11-15 14:26:55 -0600

    dirty runners for all test suites

---
+++ test/clean.sh
@@ -0,0 +1,3 @@
+cd unit; ./clean.sh; cd ../
+cd periodic; ./clean.sh; cd ../
+cd shakespeare; ./clean.sh; cd ../
---
+++ test/dirty.sh
@@ -0,0 +1,3 @@
+cd unit; ./dirty.sh; cd ../
+# cd periodic; ./dirty.sh; cd ../
+cd shakespeare; ./dirty.sh; cd ../
