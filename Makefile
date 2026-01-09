236b8635479ecef5648dbfb6221d0c8c59502e41
Eric Hanson <eric@aquameta.com>
2026-01-09 12:05:59 -0600

    remove extension building stuff

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-# Extension identity
-EXTENSION = bundle
-
-# Include centralized file list (defines SQL_FILES_STANDALONE, SQL_FILES_EXTENSION, TEST_FILES)
+MODULE = bundle
 include files.mk
-
-# Include common build logic
 include ../common.mk
--- _begin_extension.sql
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------------------------------
--- EXTENSION INITIALIZATION
-------------------------------------------------------------------------------
-
--- Extension mode: bundle schema is created by CREATE EXTENSION
--- Set search_path to bundle for the extension
-set search_path=bundle;
